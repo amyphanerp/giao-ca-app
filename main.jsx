123851a6e94d43773950d56f52ea3a26c5ea14c4
amyphanerp <amy.phan.erp@gmail.com>
2026-06-09 20:52:06 +0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../giao-ca-production.jsx'
+import App from './giao-ca-production.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
